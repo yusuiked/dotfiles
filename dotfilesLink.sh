fed4897e9a31e03bbc4f6c9841ad88e9fbca98c0
yukung <yukung.i@gmail.com>
2017-05-20 18:56:27 +0900

    Manage dotfiles with homesick.

--- dotfilesLink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitattributes_global ~/.gitattributes_global
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
