90108a5003b72e7259384999a8853d47ceeef865
yusuiked <yusuiked@gmail.com>
2023-12-17 18:05:56 +0900

    Migrated from asdf to rtx

--- home/.zsh.d/defer/lang.zsh
+++ home/.zsh.d/defer/lang.zsh
@@ -6,8 +6,8 @@ fi
 # sdkman
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# asdf
-if type asdf > /dev/null 2>&1; then
-  source $HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh
+# rtx
+if type rtx > /dev/null 2>&1; then
+  eval "$(rtx activate zsh)"
 fi
 
